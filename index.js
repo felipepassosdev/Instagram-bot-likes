51dee6a42218b6d2ce41171092b380d8df892480
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-08-18 13:37:15 -0300

    start

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+    const like = document.querySelector('span .wpO6b');
+    const next = document.querySelector('._65Bje')
+
+    like.click()
+    next.click()    
+}, 2000)  
+
